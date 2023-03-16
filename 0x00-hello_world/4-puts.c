508ffc6bbc93e74b3a07dbec6e3e2c931ba3f215
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-03-16 12:38:14 +0000

    c program that prints out text

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - starting point
+*return: if returns 0, it means success
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
