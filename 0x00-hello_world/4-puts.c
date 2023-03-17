9f941cdd20e785866b30af11d2eab074509ba072
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-03-17 05:21:05 +0000

    program that prints put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-*main - starting point
-*return: 0 return means successful output
+*main - A program that prints a line with puts function
+*return: Always 0 (success)
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
