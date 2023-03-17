69687b7deb91d43d96007f1e063d01b0c0ce3323
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-03-17 06:05:56 +0000

    unix print

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <stdlib>
+#include <unistd.h>
 /**
- *main - Entry point
+ *main - A c pogram that prints a line to the standard error
  *Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
