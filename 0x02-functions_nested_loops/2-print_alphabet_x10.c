dcc02a09eefc8e18248ea684b926481aa1bfac1a
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-03-21 16:22:22 +0000

    print multiple

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**main - prints 10 times alphabet
+ *Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+int i = 0;
+char c;
+while (1 <= 9)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+i++;
+}
+}
