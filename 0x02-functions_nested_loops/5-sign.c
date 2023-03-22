822138b665e87d4cedc564dcfc03a936d06be8ca
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-03-22 03:57:29 +0000

    number greater than, less or equal to 0

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* print_sign - check if the input number is greater, equal of less than zero
+*@n: The input as an integer
+*Return: 1 is greater than zero. 0 is zero and -1 is less than zero
+*/
+print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n < 0)
+{
+_putchar(45);
+return (-1);
+}
+else
+{
+_putchar(48);
+return (9);
+}
+_putchar('\n');
+}
