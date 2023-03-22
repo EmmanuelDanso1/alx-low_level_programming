464cfdc3ba03f8b02805da2cb0a3c821e168915d
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-03-22 07:41:09 +0000

    printing 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,7 +5,7 @@
 */
 void times_table(void)
 {
-int x, y, z, i, j
+int x, y, z, i, j;
 for (x = 0; x <= 9; x++)
 {
 for (y = 0; y <= 9; y++)
@@ -25,11 +25,12 @@ else
 if (y != 0)
 {
 _putchar(44);
-_putchar(32;
+_putchar(32);
 _putchar(32);
 }
 _putchar(z + '0');
 }
 }
 _putchar('\n');
+}
 }
