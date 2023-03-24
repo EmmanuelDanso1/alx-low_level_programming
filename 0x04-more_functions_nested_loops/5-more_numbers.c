19586acf294e9be48a6aa43e6e0fbce09c3b976b
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-03-24 07:16:30 +0000

    printing numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*more_functions - Print 10 times the numbers form 0 to 14
+*more_numbers - Print 10 times the numbers form 0 to 14
 *Return: 10 times of thr number from 0 to 14
 */
 void more_numbers(void)
