a7f3c04bd84bce69c35fa2e4fe8a6cefe51ce7ec
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-04-19 21:27:40 +0000

    prints name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,7 +6,7 @@
 *@f: pointer to function
 *Return: nothinh
 */
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
 if (name == NULL || f == NULL)
 return;
