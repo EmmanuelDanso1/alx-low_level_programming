354c24e684f7d5cf78e2a7dcbb654adfacdc6a13
EmmanuelDanso1 <kwekudanso21@gmail.com>
2023-06-15 17:34:16 +0000

    main header function

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *malloc_checked - allocates memory using memory allocation
+ * @b: number of bytes to be allocated
+ * Return: a pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+void *ptr;
+ptr = malloc(b);
+if (ptr == NULL)
+exit(98);
+return (ptr);
+}
