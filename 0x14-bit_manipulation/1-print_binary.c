cd04a1b3cdcc391e92f3554606ae01bda8b96ca8
EmmanuelDanso1 <kwekudanso21@gmail.com>
2024-02-08 16:41:26 +0000

    function that prints the binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,28 +1,46 @@
 #include "main.h"
 /**
-* print_binary - Prints the binary representation of a number
-* @n: The number to print in binary
+* _pow - func calculates (base ^ power)
+* @base: base of the exponent
+* @power: power of the exponent
+*
+* Return: value of (base ^ power)
+*/
+unsigned long int _pow(unsigned int base, unsigned int power)
+{
+unsigned long int num = 0;
+unsigned int cnt = 1;
+while (cnt < power)
+{
+num *= base;
+cnt++;
+}
+return (num);
+}
+/**
+* print_binary - prints a number in binary notation
+* @n: number to print
+*
+* Return: void
 */
 void print_binary(unsigned long int n)
 {
-unsigned long int m = 1UL << ((sizeof(unsigned long int) * 8) - 1);
-int std = 0;
-if (n == 0)
+unsigned long int div, chk;
+char flg;
+flg = 0;
+div = _pow(2, sizeof(unsigned long int) * 8 - 1);
+while (div != 0)
 {
-putchar(48);
-return;
+chk = n & div;
+if (chk == div)
+{
+flg = 1;
+_putchar('1');
 }
-while (m)
+else if (flg == 1 || div == 1)
 {
-if (n & m)
-{
-putchar('1');
-std = 1;
+_putchar('0');
 }
-else if (std)
-{
-putchar(48);
-}
-m >>= 1;
+div >>= 1;
 }
 }
