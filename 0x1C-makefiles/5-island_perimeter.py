a24befc75b5a62458be1add924d8dd4aaeff9ad9
emmanueldanso1 <kwekudanso21@gmail.com>
2024-05-29 09:27:45 -0700

    get the perimeter of the grid of island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,6 +11,6 @@ def island_perimeter(grid):
                 size += 1
                 if (j > 0 and grid[i][j - 1] == 1):
                     edges += 1
-                if (i > 0 and grid(i - 1][j] == 1):
+                if (i > 0 and grid[i - 1][j] == 1):
                     edges += 1
-    return ((sizes * 4) -(edges * 2))
+    return ((sizes * 4) - (edges * 2))
